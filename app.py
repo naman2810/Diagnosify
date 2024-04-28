49b96681732f1bcf143a928afa383c78916fbfa0
naman2810 <nishilranka28@gmail.com>
2024-04-28 23:24:39 +0530

    .

--- app.py
+++ app.py
@@ -32,6 +32,56 @@ if authentication_status :
     
     diabetes_model = pickle.load(open("Diabetes_Model.sav", 'rb'))
 
+    def show_index_page():
+
+        st.title('Diabetes')
+        st.image("image/diabetes.png", use_column_width=True)
+        st.markdown('Diabetes is a disease that occurs when your blood glucose, also called blood sugar, is too high. Blood glucose is your main source of energy and comes from the food you eat. Insulin, a hormone made by the pancreas, helps glucose from food get into your cells to be used for energy.')
+
+        st.header('What health problems can people with diabetes develop?')
+        st.markdown('Over time, high blood glucose leads to problems such as:')
+        st.markdown('- Heart disease')
+        st.markdown('- Stroke')
+        st.markdown('- Kidney disease')
+        st.markdown('- Eye problems')
+        st.markdown('- Dental disease')
+        st.markdown('- Nerve damage')
+        st.markdown('- Foot problems')
+
+        st.header('How to manage diabetes?')
+        st.markdown('To manage diabetes effectively, individuals can:')
+        st.markdown('- Maintain a healthy diet')
+        st.markdown('- Engage in regular physical activity')
+        st.markdown('- Monitor blood glucose levels regularly')
+        st.markdown('- Take prescribed medications as directed by healthcare providers')
+
+
+        st.markdown('---')
+
+        # Heart Disease section
+        st.title('Heart Disease')
+        st.image("image/heart.png", use_column_width=True)
+        st.header('Overview')
+        st.markdown('Heart disease describes a range of conditions that affect your heart. Diseases under the heart disease umbrella include blood vessel diseases, such as coronary artery disease; heart rhythm problems (arrhythmias); and heart defects you\'re born with (congenital heart defects), among others.')
+        st.markdown('The term "heart disease" is often used interchangeably with the term "cardiovascular disease." Cardiovascular disease generally refers to conditions that involve narrowed or blocked blood vessels that can lead to a heart attack, chest pain (angina) or stroke. Other heart conditions, such as those that affect your heart\'s muscle, valves or rhythm, also are considered forms of heart disease.')
+        st.markdown('Many forms of heart disease can be prevented or treated with healthy lifestyle choices.')
+
+        st.header('Symptoms')
+        st.markdown('Common symptoms of heart disease include:')
+        st.markdown('- Chest pain, chest tightness, chest pressure and chest discomfort (angina)')
+        st.markdown('- Shortness of breath')
+        st.markdown('- Pain, numbness, weakness or coldness in your legs or arms if the blood vessels in those parts of your body are narrowed')
+        st.markdown('- Pain in the neck, jaw, throat, upper abdomen or back')
+
+        st.header('Prevention')
+        st.markdown('To prevent heart disease, individuals can:')
+        st.markdown('- Maintain a healthy diet low in saturated fats, cholesterol, and sodium')
+        st.markdown('- Engage in regular physical activity')
+        st.markdown('- Avoid smoking and limit alcohol consumption')
+        st.markdown('- Manage stress effectively')
+
+        
+        
     # Function to take user inputs for heart disease prediction
     def get_heart_user_inputs():
         st.subheader('Heart Disease - User Input Features')
@@ -100,12 +150,12 @@ if authentication_status :
     # Title and description
     st.title('Health Prediction')
     
-    # authenticator.logout("logout","sidebar")
+    authenticator.logout("logout","sidebar")
             
     
     st.sidebar.title(f"Welcome {name}")
     # Sidebar tabs for heart disease and diabetes
-    selected_tab = st.sidebar.selectbox('Select prediction type:', ('Heart Disease', 'Diabetes'))
+    selected_tab = st.sidebar.selectbox('Select prediction type:', ('index','Heart Disease', 'Diabetes',))
     # Prediction based on selected tab
     if selected_tab == 'Heart Disease':
         st.header('Heart Disease Prediction')
@@ -117,7 +167,7 @@ if authentication_status :
                 st.write('Person does not have heart disease.')
             else:
                 st.write('Person has high chances of heart disease')
-    else:
+    elif selected_tab== 'Diabetes':
         st.header('Diabetes Prediction')
         user_input = get_diabetes_user_inputs()
         st.write(user_input)
@@ -127,3 +177,5 @@ if authentication_status :
                 st.write('Person does not have diabetes.')
             else:
                 st.write('Person has high chances of diabetes')
+    else:
+        show_index_page()
